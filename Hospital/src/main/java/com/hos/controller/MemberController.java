13124ba81dd98f38b57494402ca7b13f44aeccfd
RhoHwangMin <shghkdals@gmail.com>
2023-11-27 20:48:03 +0900

    메인 - 의료진 페이지 만드는중

--- Hospital/src/main/java/com/hos/controller/MemberController.java
+++ Hospital/src/main/java/com/hos/controller/MemberController.java
@@ -59,9 +59,6 @@ public class MemberController {
 		logger.info("로그인 페이지 진입");
 
 	}
-<<<<<<< HEAD
-
-=======
 	
 	// 마이 페이지 이동
 	@RequestMapping(value = "mypage", method = RequestMethod.GET)
@@ -70,7 +67,6 @@ public class MemberController {
 		logger.info("마이 페이지 진입");
 	}
 	
->>>>>>> branch 'main' of https://github.com/NoKyumHak/Hospital
 	// 회원가입
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
 	public String joinPOST(MemberVO member) throws Exception {
