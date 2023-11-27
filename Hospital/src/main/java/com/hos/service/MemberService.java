8aa4bae4543dad0148d9e1bddf287c04b8b09987
NoKyumHak <qkr7181234@gmail.com>
2023-11-27 21:22:09 +0900

    clean

--- Hospital/src/main/java/com/hos/service/MemberService.java
+++ Hospital/src/main/java/com/hos/service/MemberService.java
@@ -22,15 +22,8 @@ public interface MemberService {
 	
 	// 로그인
     public MemberVO memberLogin(MemberVO member) throws Exception;
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> branch 'main' of https://github.com/NoKyumHak/Hospital
     // 예약 
     public MemberVO checkGetDetail(MemberVO member) throws Exception;
-<<<<<<< HEAD
 
-=======
->>>>>>> branch 'main' of https://github.com/NoKyumHak/Hospital
 }
